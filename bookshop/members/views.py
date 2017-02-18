c49b0d53b2378c95fb47d4cb110a229eef4c218a
Spin Lai <spin_lai@trend.com.tw>
2017-02-18 16:57:13 +0800

    Remove unused import

--- bookshop/members/views.py
+++ bookshop/members/views.py
@@ -1,5 +1,4 @@
 from django.views.generic import DetailView
-from django.urls import reverse
 
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
