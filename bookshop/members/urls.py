6da7092309e81226f381b6203ac9da296adcd5be
Spin Lai <spin_lai@trend.com.tw>
2017-02-19 00:34:48 +0800

    Implement user registration and success message view.

--- bookshop/members/urls.py
+++ bookshop/members/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from members import views as views
 
 urlpatterns = [
-    url(r'^profile/(?P<pk>\d+)/$', views.member_profile_view, name='member_profile')
+    url(r'^profile/(?P<pk>\d+)/$', views.member_profile_view, name='member_profile'),
+    url(r'^registration/$', views.member_registration_view, name='registration'),
+    url(r'^registration/success/$', views.success_message_view, name="success")
 ]
--- bookshop/members/views.py
+++ bookshop/members/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView, TemplateView
+from django.urls import reverse
 
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from members.forms import RegistrationForm
 
 
 class MemberProfileView(LoginRequiredMixin, DetailView):
@@ -9,4 +12,19 @@ class MemberProfileView(LoginRequiredMixin, DetailView):
     template_name = 'members/profile.html'
 
 
+class RegistrationView(LoginRequiredMixin, CreateView):
+    form_class = RegistrationForm
+    model = User
+    template_name = 'members/registration.html'
+
+    def get_success_url(self):
+        return reverse('success')
+
+
+class SuccessMessageView(LoginRequiredMixin, TemplateView):
+    template_name = 'members/registration_success.html'
+
+
 member_profile_view = MemberProfileView.as_view()
+member_registration_view = RegistrationView.as_view()
+success_message_view = SuccessMessageView.as_view()
